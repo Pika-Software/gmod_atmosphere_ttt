2edaabd8e79383a492eae6f8806885201ad62ecd
PrikolMen <olegis019@gmail.com>
2022-09-12 19:04:11 +0300

    fixes & speedup

--- lua/autorun/sh_atmosphere_ttt.lua
+++ lua/autorun/sh_atmosphere_ttt.lua
@@ -1,38 +1,43 @@
-if (SERVER) or engine.ActiveGamemode() ~= "terrortown" then return end
+if (SERVER) then return end
+if engine.ActiveGamemode() == "terrortown" then
 
-require( "atmosphere" )
+    require( "atmosphere" )
 
-local GetGlobalFloat = GetGlobalFloat
-local tostring = tostring
-local math_max = math.max
-local SysTime = SysTime
+    local atmosphere_Run = atmosphere.Run
+    local GetGlobalFloat = GetGlobalFloat
+    local timer_Create = timer.Create
+    local math_floor = math.floor
+    local math_max = math.max
+    local tostring = tostring
+    local CurTime = CurTime
 
-local addonName = "Atmosphere TTT"
-local function Start()
-    timer.Create(addonName, 1, 3, function()
-        atmosphere.Run( "discord.rpc.setupRoundTime", tostring( math_max( 0, GetGlobalFloat( "ttt_round_end", 0 ) - SysTime() ) ) )
+    local addonName = "Atmosphere TTT"
+    local function Update()
+        timer_Create(addonName, 1, 3, function()
+            local time = math_max( 0, math_floor( GetGlobalFloat( "ttt_round_end", 0 ) - CurTime() ) )
+            if (time > 1) then
+                atmosphere_Run( "discord.rpc.setupRoundTime", tostring( time ) )
+            else
+                atmosphere_Run( "discord.rpc.clearGameTime" )
+            end
+        end)
+    end
+
+    local hook_Add = hook.Add 
+    hook_Add("TTTBeginRound", addonName, function()
+        Update()
     end)
+
+    hook_Add("TTTPrepareRound", addonName, function()
+        Update()
+    end)
+
+    hook_Add("TTTEndRound", addonName, function()
+        Update()
+    end)
+
+    hook_Add("InitPostEntity", addonName, function()
+        Update()
+    end)
+
 end
-
-local function Stop()
-    atmosphere.Run( "discord.rpc.clearGameTime" )
-    timer.Remove( addonName )
-end
-
-local hook_Add = hook.Add
-
-hook_Add("TTTBeginRound", addonName, function()
-    Start()
-end)
-
-hook_Add("TTTPrepareRound", addonName, function()
-    Start()
-end)
-
-hook_Add("TTTEndRound", addonName, function()
-    Start()
-end)
-
-hook_Add("InitPostEntity", addonName, function()
-    Stop()
-end)
