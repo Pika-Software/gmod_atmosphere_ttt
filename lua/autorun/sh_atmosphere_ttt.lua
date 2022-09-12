9800b5e215b5ed4cc050f3c6d1422dcb1d18511d
PrikolMen <olegis019@gmail.com>
2022-09-12 18:47:03 +0300

    Atmosphere TTT

--- lua/autorun/sh_atmosphere_ttt.lua
+++ lua/autorun/sh_atmosphere_ttt.lua
@@ -1,22 +1,38 @@
 if (SERVER) or engine.ActiveGamemode() ~= "terrortown" then return end
 
-local GetGlobalFloat = GetGlobalFloat
-local hook_Add = hook.Add
-local tostring = tostring
-local math_max = math.max
-local CurTime = CurTime
-
 require( "atmosphere" )
 
+local GetGlobalFloat = GetGlobalFloat
+local tostring = tostring
+local math_max = math.max
+local SysTime = SysTime
+
 local addonName = "Atmosphere TTT"
+local function Start()
+    timer.Create(addonName, 1, 3, function()
+        atmosphere.Run( "discord.rpc.setupRoundTime", tostring( math_max( 0, GetGlobalFloat( "ttt_round_end", 0 ) - SysTime() ) ) )
+    end)
+end
+
+local function Stop()
+    atmosphere.Run( "discord.rpc.clearGameTime" )
+    timer.Remove( addonName )
+end
+
+local hook_Add = hook.Add
+
 hook_Add("TTTBeginRound", addonName, function()
-    atmosphere.Run( "discord.rpc.setupRoundTime", tostring( math_max( 0, GetGlobalFloat( "ttt_round_end", 0 ) - CurTime() ) ) )
+    Start()
 end)
 
 hook_Add("TTTPrepareRound", addonName, function()
-    atmosphere.Run( "discord.rpc.clearGameTime" )
+    Start()
 end)
 
 hook_Add("TTTEndRound", addonName, function()
-    atmosphere.Run( "discord.rpc.clearGameTime" )
+    Start()
+end)
+
+hook_Add("InitPostEntity", addonName, function()
+    Stop()
 end)
